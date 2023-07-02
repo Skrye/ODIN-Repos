29070e25b90000a9521edb706b6b553e5bc472f9
Skrye <16295244+Skrye@users.noreply.github.com>
2023-07-02 16:03:35 -0400

    Add New Etch-A-Sketch button

--- etch-a-sketch/etchASketch.js
+++ etch-a-sketch/etchASketch.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
 
 function sketchGrid(number) {
+container.innerHTML = "";
 	for (let i = 0; i < number; i++) {
 		let column = document.createElement("div");
 		column.classList.add("column");
@@ -18,3 +19,12 @@ function sketchGrid(number) {
 }
 
 sketchGrid(16); //initial grid of 16 squares
+
+function newGrid() {
+	let gridSize = prompt(16, "Enter a number between 1-100");
+	if (gridSize >= 1 && gridSize <= 100) {
+		sketchGrid(gridSize);
+	} else {
+		newGrid();
+	}
+}
