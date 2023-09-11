361102f7821ed0c6764cf0ec3138134af9f751c9
Skrye <16295244+Skrye@users.noreply.github.com>
2023-09-10 20:18:21 -0400

    add error messages for email field

--- Signup-forms/forms.js
+++ Signup-forms/forms.js
@@ -1,14 +1,28 @@
 const firstName = document.getElementById('first-name');
 const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
+const emailError = document.querySelector('#mail + span.error');
 const phone = document.getElementById('phone-number');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm-password');
 
 email.addEventListener('input', (event) => {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity('Must be a valid email address.');
+    if (email.validity.valid) {
+        email.textContent = '';
+        email.className = 'error';
     } else {
-        email.setCustomValidity('');
+        showError();
+        event.preventDefault();
     }
-})
+});
+
+function showError() {
+    if (email.validity.valueMIssing) {
+        email.textContent = "You need to enter an email address.";
+    } else if (email.validity.typeMismatch) {
+        emailError.textContent = 'Entered value needs to be an email address.';
+    } else if (email.validity.tooShort) {
+        emailError.textContent = `Email should be at ${email.minLength} characters; you entered ${email.value.length}.`;
+    }
+    emailError.className = "error active";
+}
