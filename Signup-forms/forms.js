85ad0c97cfbb4ede5fdae0dcf97a8d7b6c625e3e
Skrye <16295244+Skrye@users.noreply.github.com>
2023-09-16 19:41:03 -0400

    fix mobile formatting

--- Signup-forms/forms.js
+++ Signup-forms/forms.js
@@ -84,9 +84,9 @@ const mobileElements = [flexBox, logoContainer, logoBackground, logoBackgroundSo
 
 window.addEventListener('resize', () => {
     if (window.innerWidth < 860) {
-        mobileElements.forEach(classList.add('mobile'))
+        mobileElements.forEach().classList.add('mobile')
     }
     if (window.innerWidth > 860) {
-        mobileElements.forEach(classList.remove('mobile'))
+        mobileElements.forEach().classList.remove('mobile')
     }
 })
