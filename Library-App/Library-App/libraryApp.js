dedeec0fa00d32ae2d73525681eb60e2675a362e
Skrye <16295244+Skrye@users.noreply.github.com>
2023-09-27 22:29:48 -0400

    added js for library display function

--- Library-App/Library-App/libraryApp.js
+++ Library-App/Library-App/libraryApp.js
@@ -32,12 +32,24 @@ const libraryDisplay = document.getElementById('library-table');
 
 function displayLibrary() {
     for (let i = 0; i < myLibrary.length; i++) {
-        let newRow = document.createElement('tr');
-        newRow.setAttribute('id', `Book ${i}`);
-        libraryDisplay.appendChild(newRow);
-        let titleTable = document.createElement('td');
-        titleTable.innerHTML = `${myLibrary[i].title}`;
-        newRow.appendChild(titleTable);
+        let newBookRow = document.createElement('tr');
+        newBookRow.setAttribute('id', `Book ${i}`);
+        libraryDisplay.appendChild(newBookRow);
+        let titleCell = document.createElement('td');
+        titleCell.innerHTML = `${myLibrary[i].title}`;
+        newBookRow.appendChild(titleCell);
+        let authorCell = document.createElement('td');
+        authorCell.innerHTML = `${myLibrary[i].author}`;
+        newBookRow.appendChild(authorCell);
+        let pageCountCell = document.createElement('td');
+        pageCountCell.innerHTML = `${myLibrary[i].pageCount}`;
+        newBookRow.appendChild(pageCountCell);
+        let publishedDateCell = document.createElement('td');
+        publishedDateCell.innerHTML = `${myLibrary[i].publishedDate}`;
+        newBookRow.appendChild(publishedDateCell);
+        let genreCell = document.createElement('td');
+        genreCell.innerHTML = `${myLibrary[i].genre}`;
+        newBookRow.appendChild(genreCell);
     }
 }
 
