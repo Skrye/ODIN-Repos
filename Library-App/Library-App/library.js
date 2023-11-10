b9d0351013365c4e096f6002d275fcc28000aafc
Skrye <16295244+Skrye@users.noreply.github.com>
2023-11-10 12:20:18 -0500

    linking js and styles

--- Library-App/Library-App/library.js
+++ Library-App/Library-App/library.js
@@ -9,3 +9,23 @@ showButton.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     dialog.close();
 })
+
+class Book {
+    constructor(title, author, pageCount, publishedDate, genre, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pageCount = pageCount;
+        this.publishedDate = publishedDate;
+        this.genre = genre;
+        this.isRead = isRead;
+    }
+};
+
+const theHobbit = new Book('The Hobbit', 'JRR Tolkien', 310, '1937-09-21', 'Fantasy', 'true');
+const lastBookInTheUniverse = new Book('The Last Book in the Universe', 'Rodman Philbrick', 240, '2002-03-01', 'Dystopian', 'false');
+
+let myLibrary = [theHobbit, lastBookInTheUniverse];
+
+function addBookToLibrary() {
+
+}
