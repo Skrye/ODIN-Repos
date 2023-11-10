730cd87d4a82c45e47cb355b0abf0f39dcc25576
Skrye <16295244+Skrye@users.noreply.github.com>
2023-11-10 11:39:58 -0500

    adding open and close dialog, styling

--- Library-App/Library-App/library.js
+++ Library-App/Library-App/library.js
@@ -1,3 +1,11 @@
 const dialog = document.querySelector('.modal-form');
 const showButton = document.querySelector('.open-modal');
 const closeButton = document.querySelector('.close-modal');
+
+showButton.addEventListener('click', () => {
+    dialog.showModal();
+})
+
+closeButton.addEventListener('click', () => {
+    dialog.close();
+})
