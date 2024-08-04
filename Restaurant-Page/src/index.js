a0b14259285609162db6ec74bf75c509a2402d0c
Skrye <perin63501@gmail.com>
2024-08-03 23:06:20 -0400

    finished home page style and content

--- Restaurant-Page/src/index.js
+++ Restaurant-Page/src/index.js
@@ -1,8 +1,7 @@
 import './styles.css';
-import Background from './beans-round-border.jpg';
+// import Background from './beans-round-border.jpg';
 
 function init() {
-    let nav = document.getElementById('nav-buttons');
     let homeButton = document.getElementById('home');
     let menuButton = document.getElementById('menu');
     let aboutButton = document.getElementById('about');
@@ -14,43 +13,48 @@ function init() {
 
     let reviewSection = document.createElement('div');
     reviewSection.classList.add('section');
-
     let reviewText = document.createElement('p');
-    reviewText.textContent = 
-    `This place is amazing! The food is delicious and the staff is friendly.\n
+    reviewText.innerHTML = 
+    `This place is amazing! The food is delicious and the staff is friendly.<br>
      I highly recommend the Reuben sandwich. It's the best I've ever had!`;
-    
-    let reviewCustomer = document.createElement('h3');
-    reviewCustomer.textContent = "- Joe S.";
-
+    let reviewCustomer = document.createElement('p');
+    reviewCustomer.innerHTML = "- <strong>Joe S.</strong>";
     content.appendChild(reviewSection);
     reviewSection.appendChild(reviewText);
     reviewSection.appendChild(reviewCustomer);
 
     let hourSection = document.createElement('div');
-    let hours = document.createElement('h3');
-    hourSection.classList.add('section');
-    hours.textContent = "Hours:";
-    let weekdayHours = document.createElement('p');
-    weekdayHours.textContent = 
-    'Closed Mon-Tue\n' +
-    'Wed-Fri 11am-9pm';
-    let weekendHours = document.createElement('p');
-    weekendHours.textContent = 'Sat-Sun 9am-5:30pm';
     content.appendChild(hourSection);
+    let hours = document.createElement('p');
+    hourSection.classList.add('section');
+    hours.innerHTML = "<strong>Hours:</strong>";
+    let weekdayHours = document.createElement('p');
+    weekdayHours.innerHTML = 
+    `<strong>Mon-Tue</strong> Closed<br>` +
+    `<strong>Wed-Fri</strong> 11am-9pm`;
+    let weekendHours = document.createElement('p');
+    weekendHours.innerHTML = '<strong>Sat-Sun</strong> 9am-5:30pm';
     hourSection.appendChild(hours);
     hourSection.appendChild(weekdayHours);
     hourSection.appendChild(weekendHours);
 
     let locationSection = document.createElement('div');
     locationSection.classList.add('section');
-    let location = document.createElement('h3');
-    location.textContent = "Location:";
+    let location = document.createElement('p');
+    location.innerHTML = "<strong>Location:</strong>";
     content.appendChild(locationSection);
     locationSection.appendChild(location);
     let address = document.createElement('p');
     locationSection.appendChild(address);
-    address.textContent = "V374+2V2\n" + "Tanant, Morocco";
+    address.innerHTML = "V374+2V2<br>" + "Tanant, Morocco";
+
+    homeButton.addEventListener('click', function() {
+        content.innerHTML = '';
+        content.appendChild(header);
+        content.appendChild(reviewSection);
+        content.appendChild(hourSection);
+        content.appendChild(locationSection);
+    });
 }
 
 init();
